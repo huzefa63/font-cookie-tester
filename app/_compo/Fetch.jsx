1f91097b634b8d039a10fac172c68adb59d86db9
unknown <huzefaratlam63@gmail.com>
2025-06-08 11:36:57 +0530

    init

--- app/_compo/Fetch.jsx
+++ app/_compo/Fetch.jsx
@@ -10,9 +10,29 @@ try{
     console.log(err);
 }
 }
+async function getCookie(){
+    try {
+      console.log("fetching started");
+      const res = await fetch(
+        "https://back-tester-cookie.onrender.com/cookie",
+        { credentials: "include" }
+      );
+      const data = res.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+}
     return (
-        <button onClick={handleClick} className="px-8 py-3 bg-red-500 ">click to send cookies</button>
-    )
+      <div>
+        <button onClick={handleClick} className="px-8 py-3 bg-red-500 ">
+          click to send cookies
+        </button>
+        <button onClick={getCookie} className="px-8 py-3 bg-red-500 ">
+          get cookie
+        </button>
+      </div>
+    );
 }
 
 export default Fetch
--- app/page.js
+++ app/page.js
@@ -2,14 +2,7 @@ import Image from "next/image";
 import Fetch from "./_compo/Fetch";
 
 export default async  function Home() {
-  try{
-    console.log('fetching started');
-    const res = await fetch('https://back-tester-cookie.onrender.com/cookie',{credentials:'include'});
-    const data = res.json();
-    console.log(data);
-  }catch(err){
-    console.log(err);
-  }
+  
   return (
     <div className="">
       <h1>hello welcome here</h1>
